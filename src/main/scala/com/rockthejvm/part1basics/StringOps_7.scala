0aff51126b51e22ce3ca753384dc6b3c14e8054b
Javadi <javadi91m@gmail.com>
2022-12-26 21:25:11 +0100

    String Operations and Interpolations

--- src/main/scala/com/rockthejvm/part1basics/StringOps_7.scala
+++ src/main/scala/com/rockthejvm/part1basics/StringOps_7.scala
@@ -1,6 +1,6 @@
 package com.rockthejvm.part1basics
 
-object StringOps {
+object StringOps_7 {
 
   val aString: String = "Hello, I am learning Scala"
 
@@ -21,14 +21,14 @@ object StringOps {
   val numberAsString = "2"
   val number = numberAsString.toInt
 
-  // s-interpolation
+  // s-interpolation (probably standard-interpolation)
   val name = "Alice"
   val age = 12
   val greeting = "Hello, I'm " + name + " and I am " + age + "years old."
   val greeting_v2 = s"Hello, I'm $name and I'm $age years old."
   val greeting_v3 = s"Hello, I'm $name and I will be turning ${age + 1} years old."
 
-  // f-interpolation
+  // f-interpolation (format-interpolation)
   val speed = 1.2f
   val myth = f"$name can eat $speed%2.5f burgers per minute."
 
@@ -37,5 +37,14 @@ object StringOps {
 
   def main(args: Array[String]): Unit = {
     println(escapes)
+
+    // regex
+    // Using regular expressions:
+    val theUltimateAnswer: String = "To life, the universe, and everything is 42."
+    val myRegex = """.* (\d+).*""".r
+    val myRegex(answerString) = theUltimateAnswer //> answerString  : String = 42 => note: answerString is a new val which is the actual result!!
+    val answer = answerString.toInt //> answer  : Int = 42
+    println(answer)
+
   }
 }
