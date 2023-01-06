bff75a76cfb088c64a42c2b54ee1d280cbd38a01
Javadi <javadi91m@gmail.com>
2023-01-06 22:32:36 +0100

    Abstract Classes and Traits

--- src/main/scala/com/rockthejvm/part2oop/AbstractDataTypes_7.scala
+++ src/main/scala/com/rockthejvm/part2oop/AbstractDataTypes_7.scala
@@ -1,12 +1,15 @@
 package com.rockthejvm.part2oop
 
-object AbstractDataTypes {
+object AbstractDataTypes_7 {
 
   abstract class Animal {
+    // we don't need to mention "abstract" before method and fields in an abstract class. we just need to leave them unimplemented
+    // we can have abstract fields as well.
     val creatureType: String // abstract
     def eat(): Unit
     // non-abstract fields/methods allowed
-    def preferredMeal: String = "anything" // "accessor methods"
+    def preferredMeal: String = "anything" // "accessor methods" => a method without any arguments or even parentheses 
+    // ==> previously we said that as a convention, we write getter-like methods (that do not do any actions) without parentheses
   }
 
   // abstract classes can't be instantiated
@@ -16,13 +19,15 @@ object AbstractDataTypes {
   class Dog extends Animal {
     override val creatureType = "domestic"
     override def eat(): Unit = println("crunching this bone")
-    // overriding is legal for everything
+    
+    // in Scala we can override a method with a value, only applicable for methods without arguments or even parentheses
+    // the return type of the method and value type must be the same
     override val preferredMeal: String = "bones" // overriding accessor method with a field
   }
 
   val aDog: Animal = new Dog
 
-  // traits
+  // traits => describe behavior
   trait Carnivore { // Scala 3 - traits can have constructor args
     def eat(animal: Animal): Unit
   }
@@ -43,11 +48,13 @@ object AbstractDataTypes {
 
   /*
     philosophical difference abstract classes vs traits
-    - abstract classes are THINGS
-    - traits are BEHAVIORS
+    - abstract classes are THINGS => Crocodile IS AN Animal
+    - traits are BEHAVIORS  => Carnivores are Animals that EAT meat!
    */
 
+  
   /*
+  DataTypes hierarchy in Scala
     Any
       AnyRef
         All classes we write
@@ -56,13 +63,14 @@ object AbstractDataTypes {
         Int, Boolean, Char, ...
 
 
-          scala.Nothing
+              scala.Nothing ==> Nothing can be replaced with anything (even Null) but it cannot be instantiated, the only way to produce it is to throw an exception
    */
 
   val aNonExistentAnimal: Animal = null
-  val anInt: Int = throw new NullPointerException
+//  val anInt: Int = throw new NullPointerException // throwing Exceptions return Nothing, so we can write "throw Exception" at the right-hand side of anything
 
   def main(args: Array[String]): Unit = {
 
   }
+  
 }
