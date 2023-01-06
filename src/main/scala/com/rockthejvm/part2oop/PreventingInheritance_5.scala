5b0e328f7da0922d6d6601e39574d55ab56d34e4
Javadi <javadi91m@gmail.com>
2023-01-06 16:18:46 +0100

    Preventing Inheritance

--- src/main/scala/com/rockthejvm/part2oop/PreventingInheritance_5.scala
+++ src/main/scala/com/rockthejvm/part2oop/PreventingInheritance_5.scala
@@ -1,13 +1,17 @@
 package com.rockthejvm.part2oop
 
-object PreventingInheritance {
+object PreventingInheritance_5 {
 
-  class Person(name: String) {
+  class Person(final val name: String) {
     final def enjoyLife(): Int = 42 // final = cannot be overridden
   }
 
-  class Adult(name: String) extends Person(name) {
+  // we cannot override final val
+  class Adult(/*override val*/ name: String) extends Person(name + " hi") {
     // override def enjoyLife() = 999 // illegal
+    
+    // you cannot access to super's field in the child e.g. super.name
+    // if you want to have access, you need to define them as def, not val/var
   }
 
   final class Animal // cannot be inherited
@@ -18,12 +22,15 @@ object PreventingInheritance {
   class ElectricGuitar(nStrings: Int) extends Guitar(nStrings)
   class AcousticGuitar extends Guitar(6)
 
+  // in scala, unlike Java, we're not willing to have heavy inheritance & hierarchies
+  // so by default we should think not to extend a class,
   // no modifier = "not encouraging" inheritance
   // open = specifically marked for extension
   // not mandatory, good practice (should be accompanied by documentation on what extension implies)
   open class ExtensibleGuitar(nStrings: Int)
 
   def main(args: Array[String]): Unit = {
-
+    val adult = new Adult("ah")
+    println(adult.name)
   }
 }
