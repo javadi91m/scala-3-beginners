044865c79b2deb24772ad67e982d00d446ea4932
Javadi <javadi91m@gmail.com>
2023-01-14 14:17:47 +0100

    Exercise: Extending SinglyLinkedList - filter - map - flatMap

--- src/main/scala/com/rockthejvm/practice/SinglyLList.scala
+++ src/main/scala/com/rockthejvm/practice/SinglyLList.scala
@@ -5,9 +5,20 @@ import scala.annotation.tailrec
 abstract class SinglyLList[A] {
 
   def head: A
+
   def tail: SinglyLList[A]
+
   def isEmpty: Boolean
+
   def add(element: A): SinglyLList[A] = new SinglyLinkedList[A](element, this)
+
+  def filter(predicate: PredicateL[A]): SinglyLList[A]
+
+  def map[B](transformer: TransformerL[A, B]): SinglyLList[B]
+
+  def flatMap[B](transformer: TransformerL[A, SinglyLList[B]]): SinglyLList[B]
+
+  infix def ++(anotherList: SinglyLList[A]): SinglyLList[A]
 
 }
 
@@ -20,7 +31,13 @@ class EmptyList[A]() extends SinglyLList[A] {
 
   override def toString: String = "[]"
 
+  override def filter(predicate: PredicateL[A]): SinglyLList[A] = new EmptyList[A]
 
+  override def map[B](transformer: TransformerL[A, B]): SinglyLList[B] = new EmptyList[B]
+
+  override def flatMap[B](transformer: TransformerL[A, SinglyLList[B]]): SinglyLList[B] = new EmptyList[B]
+
+  override infix def ++(anotherList: SinglyLList[A]): SinglyLList[A] = anotherList
 }
 
 // we're overriding head/tail methods (accessor methods) as class fields
@@ -33,16 +50,76 @@ class SinglyLinkedList[A](override val head: A, override val tail: SinglyLList[A
       if (list.isEmpty) accumulator
       else concatenate(s"$accumulator, ${list.head}", list.tail)
     }
+
     s"[${concatenate(s"$head", this.tail)}]"
   }
+
+  override def filter(predicate: PredicateL[A]): SinglyLList[A] =
+    if (predicate.test(head)) new SinglyLinkedList[A](head, tail.filter(predicate))
+    else tail.filter(predicate)
+
+  // implementation below is tailrec, but requires reversing at the end of the process
+  //    def filter(list: SinglyLList[A], result: SinglyLList[A]): SinglyLList[A] = {
+  //      if (list.isEmpty) result
+  //      else if (predicate.test(list.head)) filter(list.tail, new SinglyLinkedList[A](list.head, result))
+  //      else filter(list.tail, result)
+  //    }
+  //
+  //    filter(this, new EmptyList[A])
+
+  override def map[B](transformer: TransformerL[A, B]): SinglyLList[B] =
+    new SinglyLinkedList[B](transformer.transform(head), tail.map(transformer))
+
+  override def flatMap[B](transformer: TransformerL[A, SinglyLList[B]]): SinglyLList[B] =
+    transformer.transform(head) ++ tail.flatMap(transformer)
+
+  override infix def ++(anotherList: SinglyLList[A]): SinglyLList[A] =
+    if (anotherList.isEmpty) this
+    else new SinglyLinkedList[A](this.head, tail ++ anotherList)
+
+
 }
 
 object SinglyLList extends App {
 
   // compiler can infer generic types without explicitly typing them
   val list: SinglyLList[Int] = new SinglyLinkedList(10, new SinglyLinkedList(7, new SinglyLinkedList(14, new EmptyList)))
-  val list2: SinglyLList[Int] = new EmptyList().add(1).add(4).add(3).add(67).add(-1)
+  val list2: SinglyLList[Int] = new EmptyList().add(1).add(4).add(3).add(67).add(-1).add(-2).add(6)
   println(list)
-  println(list2)
+
+  val evenPredicate = new PredicateL[Int] {
+    override def test(t: Int): Boolean = t % 2 == 0
+  }
+  println(list2 ++ list)
+  println(list2.toString + " filter => " + list2.filter(x => x % 2 == 0))
+  println(list2.toString + " map => " + list2.map(x => x * 2))
+  println(list2.toString + " flatMap => " + list2.flatMap(x => new SinglyLinkedList[Int](x, new SinglyLinkedList[Int](x * 2, new EmptyList[Int]))))
 
 }
+
+trait PredicateL[T] {
+  def test(t: T): Boolean
+}
+
+trait TransformerL[A, B] {
+  def transform(a: A): B
+}
+
+
+/**
+ * Exercise: LList extension
+ *
+ * 1.  Generic trait Predicate[T] with a little method test(T) => Boolean
+ * 2.  Generic trait Transformer[A, B] with a method transform(A) => B
+ * 3.  LList:
+ * - map(transformer: Transformer[A, B]) => LList[B]
+ * - filter(predicate: Predicate[A]) => LList[A]
+ * - flatMap(transformer from A to LList[B]) => LList[B]
+ *
+ * class EvenPredicate extends Predicate[Int]
+ * class StringToIntTransformer extends Transformer[String, Int]
+ *
+ * [1,2,3].map(n * 2) = [2,4,6]
+ * [1,2,3,4].filter(n % 2 == 0) = [2,4]
+ * [1,2,3].flatMap(n => [n, n+1]) => [1,2, 2,3, 3,4]
+ */
