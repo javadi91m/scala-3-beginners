84419e9a8cfda6735f0f826bdc8dcd01059fd133
Javadi <javadi91m@gmail.com>
2023-01-14 22:28:13 +0100

    Enums

--- src/main/scala/com/rockthejvm/part2oop/Enums_12.scala
+++ src/main/scala/com/rockthejvm/part2oop/Enums_12.scala
@@ -1,11 +1,15 @@
 package com.rockthejvm.part2oop
 
-object Enums {
+object Enums_12 {
 
+  // first class support for enums has been added in scala 3
+  // we can consider enum as a datatype that have only a bunch of instances that we create within enum itself
+  // these "only possible instances" are created by using "case" keyword
+  // enums are sealed, so we cannot extend them further outside of the file
   enum Permissions {
     case READ, WRITE, EXECUTE, NONE
 
-    // add fields/methods
+    // enums are datatype, so we can add fields/methods
     def openDocument(): Unit =
       if (this == READ) println("opening document...")
       else println("reading not allowed.")
@@ -13,21 +17,23 @@ object Enums {
 
   val somePermissions: Permissions = Permissions.READ
 
-  // constructor args
+  // enums can take constructor arguments
   enum PermissionsWithBits(bits: Int) {
+    // these cases need to be instantiated early, so we need to pass the constructor args
     case READ extends PermissionsWithBits(4) // 100
     case WRITE extends PermissionsWithBits(2) // 010
     case EXECUTE extends PermissionsWithBits(1) // 001
     case NONE extends PermissionsWithBits(0) // 000
   }
 
+  // enums can have a companion object
   object PermissionsWithBits {
     def fromBits(bits: Int): PermissionsWithBits = // whatever
       PermissionsWithBits.NONE
   }
 
   // standard API
-  val somePermissionsOrdinal = somePermissions.ordinal
+  val somePermissionsOrdinal = somePermissions.ordinal // 0 => Permissions.READ has been defined as index 0 (first defined enum)
   val allPermissions = PermissionsWithBits.values // array of all possible values of the enum
   val readPermission: Permissions = Permissions.valueOf("READ") // Permissions.READ
 
