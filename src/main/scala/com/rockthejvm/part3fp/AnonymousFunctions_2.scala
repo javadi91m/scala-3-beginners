cab300592fdcaf94e8c11a8897c77f9a0aef42e5
Javadi <javadi91m@gmail.com>
2023-01-15 15:54:34 +0100

    Anonymous Functions

--- src/main/scala/com/rockthejvm/part3fp/AnonymousFunctions_2.scala
+++ src/main/scala/com/rockthejvm/part3fp/AnonymousFunctions_2.scala
@@ -1,6 +1,6 @@
 package com.rockthejvm.part3fp
 
-object AnonymousFunctions {
+object AnonymousFunctions_2 {
 
   // instances of FunctionN
   val doubler: Int => Int = new Function1[Int, Int] {
@@ -8,17 +8,28 @@ object AnonymousFunctions {
   }
 
   // lambdas = anonymous function instances
+  // the compiler rewrites this syntax to a Function1 instance
   val doubler_v2: Int => Int = (x: Int) => x * 2 // identical
   val adder: (Int, Int) => Int = (x: Int, y: Int) => x + y // new Function2[Int, Int, Int] { override def apply... }
   // zero-arg functions
   val justDoSomething: () => Int = () => 45
   val anInvocation = justDoSomething()
 
-  // alt syntax with curly braces
+
+
+  // alternative syntax with curly braces
   val stringToInt = { (str: String) =>
     // implementation: code block
     str.toInt
   }
+
+  // the normal version is as below
+  val stringToIntNormal = (str: String) => {
+    // implementation: code block
+    str.toInt
+  }
+
+
 
   // type inference
   val doubler_v3: Int => Int = x => x * 2 // type inferred by compiler
@@ -40,13 +51,19 @@ object AnonymousFunctions {
     }
   }
 
+  val superAdderr: (Int => (Int => Int)) = (x) => (y => x + y)
+  println("here:> " + superAdderr(1)(2))
+
+  // we can simply say every => introduces a function. right-hand side of => is the function implementation
   val superAdder_v2 = (x: Int) => (y: Int) => x + y
   val adding2 = superAdder_v2(2) // (y: Int) => 2 + y
   val addingInvocation = adding2(43) // 45
   val addingInvocation_v2 = superAdder_v2(2)(43) // same
 
   def main(args: Array[String]): Unit = {
+    // writes the function itself
     println(justDoSomething)
+    // it's the function invocation (result will be printed)
     println(justDoSomething())
   }
 }
